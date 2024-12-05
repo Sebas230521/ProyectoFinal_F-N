b3b910b3a8e3d1aac9be0eab7ee4c91616c1b018
YU5U5 <samuelosoriogaspar@gmail.com>
2024-12-05 10:15:03 -0500

    segundo comimit

--- login_fish_nexus/urls.py
+++ login_fish_nexus/urls.py
@@ -15,13 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from login.views import LoginView 
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', include('login.urls'))
 ]
 
-urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
-]
