779deff4107a3e7476feb1e1df619e165d3e6190
Andrestapasco <brstty123@gmail.com>
2025-02-16 21:39:06 -0500

    Actualizacion 1

--- PROJECT_FN/frontend/src/App.vue
+++ PROJECT_FN/frontend/src/App.vue
@@ -1,22 +1,15 @@
 <template>
-  <appHeader/>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <appPie/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-import appHeader from './components/appHeader.vue'
-import appPie from './components/appPie.vue';
-
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    appHeader,
-    appPie
+    HelloWorld
   }
 }
 </script>
@@ -27,7 +20,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #1268b3;
+  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
