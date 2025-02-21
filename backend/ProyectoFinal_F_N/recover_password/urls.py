78febfb6723673894996f9c2e476f063b4752b3f
IDarkness28 <andresramirez210@outlook.es>
2025-02-21 13:55:16 -0500

    Carpetas Eliminadas

--- backend/ProyectoFinal_F_N/recover_password/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import SolicitarRecuperacion, ConfirmarCodigo, CambiarContraseña
-
-urlpatterns = [
-    path('solicitar/', SolicitarRecuperacion.as_view(), name='solicitar_recuperacion'),
-    path('confirmar/', ConfirmarCodigo.as_view(), name='confirmar_codigo'),
-    path('cambiar/', CambiarContraseña.as_view(), name='cambiar_contraseña'),
-]
