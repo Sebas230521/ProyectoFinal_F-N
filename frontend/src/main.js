70bf18963979452d7bfbf007e6bd9eec1a338934
Andrestapasco <brstty123@gmail.com>
2025-03-13 10:58:47 -0500

    actu8

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,8 +7,6 @@ import 'bootstrap'
 import '@/assets/css/bootstrap.min.css'
 import '@/assets/js/bootstrap.bundle.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.css';
-
 import '@fortawesome/fontawesome-free/css/all.css';
 
 createApp(App).use(router).mount('#app')
